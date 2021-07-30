f844387e502c6810d3b4ba291fb56cb07474958b
nicolle sturzbecher <nicolle.pinto@zup.com.br>
2021-07-29 23:28:26 -0300

    begining

---
+++ src/main/java/com/api/fatherson_append/FatherSonAppendApplication.java
@@ -0,0 +1,13 @@
+package com.api.fatherson_append;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatherSonAppendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatherSonAppendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/fatherson_append/FatherSonAppendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.fatherson_append;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatherSonAppendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
